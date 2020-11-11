7e2d7f8de4a11eafbf261feb72b49260805a232f
Gabriel Kim <gabrielkim13@gmail.com>
2020-11-10 21:27:02 -0300

    Add errors and middlewares

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/request-validation-error';
+export * from './errors/unauthorized-error';
+
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
