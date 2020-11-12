bb93a282f55ffaa7c438f3d2a73a7279f918247f
Gabriel Kim <gabrielkim13@gmail.com>
2020-11-12 09:29:20 -0300

    Add logging of unexpected errors

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -7,6 +7,8 @@ export function errorHandler(err: Error, req: Request, res: Response, next: Next
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  console.error(err);
+
   res.status(400).send({
     errors: [{ message: 'Something went wrong' }]
   });
