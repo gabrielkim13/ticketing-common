fe7a2d33a5c1f95f61330054905af061ed36f0ec
Gabriel Kim <gabrielkim13@gmail.com>
2020-11-15 20:38:59 -0300

    Fix TicketUpdated event

--- src/events/events.ts
+++ src/events/events.ts
@@ -11,7 +11,7 @@ export interface TicketCreatedEvent {
 }
 
 export interface TicketUpdatedEvent {
-  subject: Subjects.TicketCreated;
+  subject: Subjects.TicketUpdated;
   data: {
     id: string;
     title: string;
